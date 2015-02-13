b6c1f604d669974d3e70e608be836a058f7c4108
cyd dalupan <cydmdalupan@gmail.com>
2015-02-13 16:35:29 +0800

    Update Cdf.php

--- src/Cdf.php
+++ src/Cdf.php
@@ -1,9 +1,9 @@
-<?php namespace datemodify;
+<?php namespace cyddateformat
 
 class Cdf{
 	//use for facebook date to sql datetime
 	//from 10/08/1990 to 1990-08-10
-	function convert_date_dmy_2_ymd($mdate){
+	public static function convert_date_dmy_2_ymd($mdate){
 		$expdate = explode('/', $mdate);
 		$result = $expdate[2].'-'.$expdate[0].'-'.$expdate[1];
 		return $result;
