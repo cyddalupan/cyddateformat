5adc605e7906c0ccf66a2b716825921c7e21cd37
cyd dalupan <cydmdalupan@gmail.com>
2015-03-06 14:37:35 +0800

    new method added convert_date_mdy_2_ymd
    use for http://www.eyecon.ro/bootstrap-datepicker/
    from 08/10/1990 to 1990-08-10

--- src/Cdf.php
+++ src/Cdf.php
@@ -8,6 +8,14 @@ class Cdf{
 		$result = $expdate[2].'-'.$expdate[0].'-'.$expdate[1];
 		return $result;
 	}
+	
+	//use for http://www.eyecon.ro/bootstrap-datepicker/
+	//from 08/10/1990 to 1990-08-10
+	public static function convert_date_mdy_2_ymd($mdate){
+		$expdate = explode('/', $mdate);
+		$result = $expdate[2].'-'.$expdate[1].'-'.$expdate[0];
+		return $result;
+	}
 
 	//more convertion soon...
 }
